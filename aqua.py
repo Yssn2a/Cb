49674d5752c004729079c166b89d89f4d5d29ae5
Yssn2a <125408999+Yssn2a@users.noreply.github.com>
2025-06-10 22:59:00 +0100

    Update aqua.py

--- aqua.py
+++ aqua.py
@@ -3,7 +3,7 @@ from cf_reverse import get_cf_cookie
 
 client = httpx.Client()
 client.headers = {
-    'authority': 'www.support.kogama.com',
+    'authority': 'perchance.org',
     'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
     'accept-language': 'es-US,es-419;q=0.9,es;q=0.8',
     'sec-ch-ua': '"Not/A)Brand";v="8", "Chromium";v="129", "Google Chrome";v="129"',
@@ -18,18 +18,18 @@ client.headers = {
 }
 
 
-red_sk = client.get('https://www.support.kogama.com/')
+red_sk = client.get('https://perchance.org/')
 client.cookies = red_sk.cookies
 
 
 cf_cookie, ray = get_cf_cookie(
-    'https://www.support.kogama.com/',
-    'x3MU-7nK0tLQlyRoIXNDZOiPF+c26s$gdJAVzEv9qmapSuh5bwfjHYTk18eWBG4rC',
+    'https://perchance.org/',
+    '0x4AAAAAAAi3LdM-EVMMMFCv',
     client,
     useragent=client.headers['user-agent']
 )
 
 
 print(client.cookies)
-print('\nBYPASSED KOGAMA CLOUDFLARE')
+print('\nBYPASSED PERCHANCE CLOUDFLARE')
 
